e89c919661d5dad056be954d94dbd939c928c520
mohamad-giyast <230660121026@student.unsap.ac.id>
2026-01-08 18:08:41 +0700

    Delete public directory

--- public/admin/admin_auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-// Pastikan menggunakan variabel sesi yang sama seperti di admin-login.php
-if (!isset($_SESSION['admin_id'])) {
-    header("Location: admin-login.php");
-    exit();
-}
-?>
